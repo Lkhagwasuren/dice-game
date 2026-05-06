9f70ca280a3b307358d086b9e0d1853dbc8a6200
sharaa <lkhagwal41@gmail.com>
2026-05-06 15:31:20 +0800

    title uurchilluu

---
+++ app.js
@@ -0,0 +1,18 @@
+// Тоглогчийн ээлжийг халгалах хувисагч, 1-р тоглогчийг 0,2-р тоглогчийг 1, гэж тэмдэглэе.
+var activePlayer = 1;
+// Toglogchiin tsugluulsan onoog hadgalah huwisagch
+var scores = [0, 0];
+// Toglogchiin eeljindee tsugluulj baigaa onooghadgalah huwisagch
+var roundScore = 0;
+// Shooni ali talaaraa buusniig hadgalah heregtei 1-6gesen utgaene huwisagchid sanamsargvigeer vvsgej ugnu
+
+//programm ehelhed beltgey
+document.getElementById("score-0").textContent = '0';
+document.getElementById("score-1").textContent = '0';
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
+
+document.querySelector(".dice").style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;;
+});
