169104e963e2a5c027c0ec61f78166c08d60f57b
sharaa <lkhagwal41@gmail.com>
2026-05-08 13:08:47 +0800

    shoog shiddeg bolloo

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ document.getElementById("score-1").textContent = '0';
 document.getElementById("current-0").textContent = '0';
 document.getElementById("current-1").textContent = '0';
 
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 document.querySelector(".btn-roll").addEventListener("click", function () {
-    var diceNumber = Math.floor(Math.random() * 6) + 1;;
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + '.png';
 });
