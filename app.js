0e262b23339067aa0a1f758a365faf712cdcb216
sharaa <lkhagwal41@gmail.com>
2026-05-12 10:58:11 +0800

    toglogchiin eeljiig solidog bolgon

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг халгалах хувисагч, 1-р тоглогчийг 0,2-р тоглогчийг 1, гэж тэмдэглэе.
-var activePlayer = 1;
+var activePlayer = 0;
 // Toglogchiin tsugluulsan onoog hadgalah huwisagch
 var scores = [0, 0];
 // Toglogchiin eeljindee tsugluulj baigaa onooghadgalah huwisagch
@@ -14,8 +14,41 @@ document.getElementById("current-1").textContent = '0';
 
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
+
+// Shoog shideh event listener
 document.querySelector(".btn-roll").addEventListener("click", function () {
+    // 1-6 dotorh sanamsargui neg too gargaj awna
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+    // Shoornii zuragiigweb deer gargaj irne
     diceDom.style.display = "block";
+    // buusan sanamsargui tooni hargalzah shoonii zurgiig web deer gargaj irne
     diceDom.src = "dice-" + diceNumber + '.png';
+    // buusan too n 1ees ylgaatai bol idewhtei toglogchiin  eeljiin onoog oorchilno
+    if (diceNumber !== 1) {
+        //1-ees yalgaatai too buulaa. buusan toog toglogchid nemj ogno
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    } else {
+        //1 buusan tul toglogchiin eeljiigene hesegt solij ogno
+
+        // ene toglogchiin tsugluulsan onoog 0 bolgono
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+
+        //  toglogchiin eeljiig nogoo tolgoch ruu shiljvvlne
+        //herew idewhtei toglogch 0 baiwl idewhitei toglogchiig 1 bolgo
+        // ugui bol idewhitei toglogchiig 0 bolgo
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+        // Ulaan tsegiig shiljvvleh
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+        //shoog tvr alga bolgono
+        diceDom.style.display = "none";
+
+        // if (activePlayer === 0) {
+        //     activePlayer = 1;
+        // } else {
+        //     activePlayer = 0;
+        // }
+    }
 });
